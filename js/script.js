15b8fe36574fb5408136778011430a263e8af08d
Matteo Ciancone <matteo.ciancone@gmail.com>
2022-05-10 18:31:38 +0200

    Modificata pagina prodotto

--- js/script.js
+++ js/script.js
@@ -5,32 +5,40 @@ window.onload = function hideRow6() {
 
 /*********PRODUCTS GRID DISPLAY***********/
 function displayRow4() {
-    document.querySelector(".cardRow4").style.display = ""
-    document.querySelector(".cardRow6").style.display = "none"
-    document.querySelector(".gridBtn6").style.opacity = "50%"
-    document.querySelector(".gridBtn4").style.opacity = "100%"
+    document.querySelector(".cardRow4").style.display = "";
+    document.querySelector(".cardRow6").style.display = "none";
+    document.querySelector(".gridBtn6").style.opacity = "50%";
+    document.querySelector(".gridBtn4").style.opacity = "100%";
 }
 
 function displayRow6() {
-    document.querySelector(".cardRow4").style.display = "none"
-    document.querySelector(".cardRow6").style.display = ""
-    document.querySelector(".gridBtn4").style.opacity = "50%"
-    document.querySelector(".gridBtn6").style.opacity = "100%"
+    document.querySelector(".cardRow4").style.display = "none";
+    document.querySelector(".cardRow6").style.display = "";
+    document.querySelector(".gridBtn4").style.opacity = "50%";
+    document.querySelector(".gridBtn6").style.opacity = "100%";
 }
+
+document.querySelector(".gridBtn4").addEventListener("click", displayRow4);
+document.querySelector(".gridBtn6").addEventListener("click", displayRow6);
+
 
 /************CARD CLONE**********/
 
 const card = document.querySelector(".cardColGrid4");
 
 for (let i = 0; i < 15; i++) {
+    let n = 1;
     let copy = card.cloneNode(true);
-    card.parentNode.appendChild(copy)
+    copy.id = "card" + " " + n++;
+    card.parentNode.appendChild(copy);
 }
-
 
 const card2 = document.querySelector(".cardColGrid6");
 
-for (let i = 0; i < 17; i++) {
+for (let i = 0; i < 15; i++) {
+    let n = 1;
     let copy2 = card2.cloneNode(true);
-    card2.parentNode.appendChild(copy2)
+    copy2.id = "card" + " " + n++;
+    card2.parentNode.appendChild(copy2);
 }
+

c2142256ddf2f99f8ec98bce8569bf0a7364d698
Matteo Ciancone <matteo.ciancone@gmail.com>
2022-05-13 18:15:19 +0200

    Implementato swipe rimosso flickity

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,40 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".swiperSp", {
     scrollbar: {
         el: ".swiper-scrollbar",
         hide: true,
+    },
+    breakpoints: {
+
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+        },
+    },
+});
+
+
+var swiper = new Swiper(".swiperService", {
+    loop: true,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 5,
+        },
+        425: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 5,
+        },
+        1024: {
+            slidesPerView: 6,
+            spaceBetween: 5,
+        }
     },
 });

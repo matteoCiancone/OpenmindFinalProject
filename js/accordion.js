fb313c8d55cf9f548a1cd3bb9df1038d9eea7c58
Matteo Ciancone <matteo.ciancone@gmail.com>
2022-05-16 17:10:52 +0200

    Corretto in session storage

--- js/accordion.js
+++ js/accordion.js
@@ -17,10 +17,19 @@ acc.forEach(btn => {
 const newsLetterBtn = document.querySelector("#newsLetterBtn");
 const newsLetter = document.querySelector(".newsLetter");
 
+if (sessionStorage.getItem("newsLetter")) {
+    hideNewsLetter();
+}
+
+function populateStorage() {
+    sessionStorage.setItem("newsLetter", newsLetter);
+    hideNewsLetter();
+}
+
 function hideNewsLetter() {
     newsLetter.setAttribute("hidden", "hidden");
 }
 
-newsLetterBtn.addEventListener("click", hideNewsLetter);
+newsLetterBtn.addEventListener("click", populateStorage);
 
 
